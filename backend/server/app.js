a0715dec0f5edb0ef99073898b8027e046855e84
chaeheetae94 <chaeheetae94@gmail.com>
2022-10-23 21:41:25 +0900

    'f'

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,12 +1,35 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 
 const product = require("./routes/product");
+const post = require("./routes/post");
 
 const app = express();
 
 const PORT = process.env.PORT || 3060;
 
+mongoose
+  .connect(
+    "mongodb+srv://HeetaeChae:1234@cluster0.4sz9uja.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("mongoDB is connected"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use("/", express.static("./public"));
+
 app.use("/api/product", product);
+app.use("/api/post", post);
 
 app.listen(PORT, () => {
   console.log(`Server is running in ${PORT}`);
